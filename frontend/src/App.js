86ca4428440e37c80ee96eabfa8785b65619f96b
Timothy Chang <timothychang@ucsb.edu>
2022-08-09 16:54:54 -0700

    installed frontend dependencies +  added homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import HomePage from './Pages/Home/HomePage.js';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Router>
+        <Routes>
+          <Route path='/' element={<HomePage/>}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
