baae7d60d96e3cbea1399829a940365ad4f099df
Timothy Chang <timothychang@ucsb.edu>
2022-08-31 01:38:17 -0700

    update

--- server.js
+++ server.js
@@ -16,15 +16,19 @@ dotenv.config();
 
 const app = express();
 app.use(cors());
-
 connectDB();
+
 app.use(express.json({limit:'1mb'}));
+
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
+//app.use(express.json({limit:'1mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit:'1mb' }));
 
 
 app.use('/', roomRoutes);
 
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+//app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
