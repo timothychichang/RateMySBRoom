cc5c2d7b90c1a56e62257f0089c5db158e3a0461
Timothy Chang <timothychang@ucsb.edu>
2022-08-31 01:20:55 -0700

    rearranged

--- server.js
+++ server.js
@@ -21,9 +21,10 @@ connectDB();
 app.use(express.json({limit:'1mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit:'1mb' }));
 
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.use('/', roomRoutes);
+
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
