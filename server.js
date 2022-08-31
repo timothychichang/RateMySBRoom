4ba8a076dee575333694068e8528e7ae96609d6a
Timothy Chang <timothychang@ucsb.edu>
2022-08-31 01:41:00 -0700

    test

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ connectDB();
 app.use(express.json({limit:'1mb'}));
 
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-
+/*
 //app.use(express.json({limit:'1mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit:'1mb' }));
 
@@ -33,7 +33,7 @@ app.use('/', roomRoutes);
 app.get('*', function(request, response) {
     response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
 });
-
+*/
 const PORT = process.env.PORT || 8800; 
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
