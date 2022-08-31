261c5016c21ecb71c8b553a57cc1f20cf91ec106
Timothy Chang <timothychang@ucsb.edu>
2022-08-31 01:09:36 -0700

    updated

--- server.js
+++ server.js
@@ -3,8 +3,14 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import path from 'path';
+import {fileURLToPath} from 'url';
+
 import connectDB from './db.js';
 import roomRoutes from './routes/roomRoutes.js';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 
@@ -15,7 +21,13 @@ connectDB();
 app.use(express.json({limit:'1mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit:'1mb' }));
 
+app.use(express.static(path.resolve(__dirname, '../frontend/build')));
+
 app.use('/', roomRoutes);
+
+app.get('*', function(request, response) {
+    response.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 8800; 
 
